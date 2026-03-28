0a2e3636c3ad98e5947689829e9690cca32848e9
Menez sasha <s.menez@student.isartdigital.com>
2026-03-28 03:03:25 +0100

    update: terrain generation

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -1,33 +1,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class TerrainPrefabs
+{
+    public List<GameObject> tiles;
+
+    public GameObject GetRandomTile()
+    {
+        int rand = Random.Range(0, tiles.Count);
+        return tiles[rand];
+    }
+}
 public class TerrainGeneration : MonoBehaviour
 {
-    public List<GameObject> topLeft = new List<GameObject>();
-    public List<GameObject> top = new List<GameObject>();
-    public List<GameObject> topRight = new List<GameObject>();
-    public List<GameObject> left = new List<GameObject>();
-    public List<GameObject> center = new List<GameObject>();
-    public List<GameObject> right = new List<GameObject>();
-    public List<GameObject> bottomLeft = new List<GameObject>();
-    public List<GameObject> bottom = new List<GameObject>();
-    public List<GameObject> bottomRight = new List<GameObject>();
+    public TerrainPrefabs topLeft;
+    public TerrainPrefabs top;
+    public TerrainPrefabs topRight;
+    public TerrainPrefabs left;
+    public TerrainPrefabs center;
+    public TerrainPrefabs right;
+    public TerrainPrefabs bottomLeft;
+    public TerrainPrefabs bottom;
+    public TerrainPrefabs bottomRight;
+    //The first item of the list is the top left terrain. It goes from left to right, the from top to bottom.
+    private List<TerrainPrefabs> terrains = new List<TerrainPrefabs>();
+    public int numLines = 6;
+    public int numColumns = 6;
+    
+    public GameObject greenHotel;
+    public GameObject redHotel;
     void Start()
     {
-        GenerateTerrain(topLeft, new Vector3(-40, 0, 60));
-        GenerateTerrain(top, new Vector3(0, 0, 60));
-        GenerateTerrain(topRight, new Vector3(40, 0, 60));
-        GenerateTerrain(left, new Vector3(-40, 0, 30));
-        GenerateTerrain(center, new Vector3(0, 0, 30));
-        GenerateTerrain(right, new Vector3(40, 0, 30));
-        GenerateTerrain(bottomLeft, new Vector3(-40, 0, 0));
-        GenerateTerrain(bottom, new Vector3(0, 0, 0));
-        GenerateTerrain(bottomRight, new Vector3(40, 0, 0));
+        SortTerrains();
+        GenerateTerrain();
+    }
+
+    private void SortTerrains()
+    {
+        for(int i = 0; i < numLines; i++)
+        {
+            for(int j = 0; j < numColumns; j++)
+            {
+                if (i == 0 && j == 0)
+                    terrains.Add(topLeft);
+                else if (i == 0 && j == numColumns - 1)
+                    terrains.Add(topRight);
+                else if (i == numLines - 1 && j == 0)
+                    terrains.Add(bottomLeft);
+                else if (i == numLines - 1 && j == numColumns - 1)
+                    terrains.Add(bottomRight);
+                else if (i == 0)
+                    terrains.Add(top);
+                else if (i == numLines - 1)
+                    terrains.Add(bottom);
+                else if (j == 0)
+                    terrains.Add(left);
+                else if (j == numColumns - 1)
+                    terrains.Add(right);
+                else
+                    terrains.Add(center);
+            }
+        }
     }
     
-    void GenerateTerrain(List<GameObject> go, Vector3 position)
+    private void GenerateTerrain()
     {
-        int rand = Random.Range(0, go.Count);
-        Instantiate(go[rand], position, Quaternion.identity);
+        for (int i = 0; i < numLines; i++)
+        {
+            for (int j = 0; j < numColumns; j++)
+            {
+                GameObject randGo =  terrains[i * numLines + j].GetRandomTile();
+                //Position to modify
+                Vector3 pos = new Vector3(j * 40, 0, -i * 30);
+                Instantiate(randGo, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    public void GenerateGreenHotel()
+    {
+        int line = Random.Range(0, 3);
+        int column = Random.Range(0, 3);
+        Vector3 position = new Vector3(-40 + column * 40, 0, line * 30);
+        Instantiate(greenHotel, position, Quaternion.identity);
+    }
+    
+    public void GenerateRedHotel()
+    {
+        int line = Random.Range(0, 3);
+        int column = Random.Range(0, 3);
+        Vector3 position = new Vector3(-40 + column * 40, 0, line * 30);
+        Instantiate(redHotel, position, Quaternion.identity);
     }
 }
